5fa516f3476126ff0bbde4b6de329a6e5f123d08
YanaBytkova <nedavnyaya.yana@gmail.com>
2023-12-10 17:22:30 +0200

    fix mistakes

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,7 +2,16 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
-const page1 = {
+type Details = {createAt: Date, updateAt: Date};
+type universalPage = {
+  title: string,
+  likes: number,
+  accounts: string[],
+  status: 'close' | 'open' ,
+  details?: Details
+}
+
+const page1: universalPage = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
@@ -13,20 +22,13 @@ const page1 = {
   }
 }
 
-const page2 = {
+const page2: universalPage = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
   status: 'close',
 }
 
-type Details = {createAt:string, updateAt:string};
-type universalPage = {
-  title: string,
-  likes: number,
-  accounts: string[],
-  status: string,
-  details?: Details
-}
+
 
 export {};
